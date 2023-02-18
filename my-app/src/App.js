b5aa09836b3fdb0069152088aa59d381480a0c19
Kumar <krishav66@gmail.com>
2023-02-18 13:03:26 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import {BpkCode} from 'bpk-component-code';
+import BpkCalendar , { CALENDAR_SELECTION_TYPE } from '@skyscanner/backpack-web/bpk-component-calendar';
+import cssModules from '@skyscanner/backpack-web/bpk-react-utils';
+import {alert} from 'react-alert';
+import BpkButton from './@skyscanner/backpack-web/bpk-component-button';
+import format from 'date-fns/format';
+import BpkInput, { INPUT_TYPES } from './@skyscanner/backpack-web/bpk-component-input';
+import STYLES from './App.scss';
+
+const getClassName = cssModules(STYLES);
+const formatDateFull = date => format(date, 'EEEE, do MMMM yyyy');
+const formatMonth = date => format(date, 'MMMM yyyy');
+const daysOfWeek = [
+  {
+    name: 'Sunday',
+    nameAbbr: 'Sun',
+    index: 0,
+    isWeekend: true,
+  },
+  {
+    name: 'Monday',
+    nameAbbr: 'Mon',
+    index: 1,
+    isWeekend: false,
+  },
+  {
+    name: 'Tuesday',
+    nameAbbr: 'Tue',
+    index: 2,
+    isWeekend: false,
+  },
+  {
+    name: 'Wednesday',
+    nameAbbr: 'Wed',
+    index: 3,
+    isWeekend: false,
+  },
+  {
+    name: 'Thursday',
+    nameAbbr: 'Thu',
+    index: 4,
+    isWeekend: false,
+  },
+  {
+    name: 'Friday',
+    nameAbbr: 'Fri',
+    index: 5,
+    isWeekend: false,
+  },
+  {
+    name: 'Saturday',
+    nameAbbr: 'Sat',
+    index: 6,
+    isWeekend: true,
+  },
+];
+
+export default class App extends Component {
+  constructor () {
+    super();
+
+    this.state = {
+      selectionConfiguration: {
+        type: CALENDAR_SELECTION_TYPE.single,
+        date: null,
+        }
+    };
+  }
+
+  handleDateSelect = (date) => {
+    this.setState({
+      selectionConfiguration: {
+        type: this.props.selectionConfiguration.type,
+        date: date,
+      },
+    });
+  }
+}
+  render () {
+    return (
+      <div className={getClassName('App')}>
+    <header className={getClassName('App__header')}>
+      <div className={getClassName('App__header-inner')}>
+        <BpkText tagName="h1" textStyle="xxl" className={getClassName('App__heading')}> Flight Schedule </BpkText>
+      </div>
+      </header>
+      <div>
+         <main className={getClassName('App__main')}>
+          <div>
+          <BpkInput
+          id='dateInput'
+          type={INPUT_TYPES.text}
+          name='date'
+          value={(this.state.selectionConfiguration.date || '').toString()}
+          placeholder='Departure date'
+        />
+        <BpkCalendar
+          id='calendar'
+          onDateSelect={this.handleDateSelect}
+          formatMonth={formatMonth}
+          formatDateFull={formatDateFull}
+          daysOfWeek={daysOfWeek}
+          weekStartsOn={1}
+          changeMonthLabel="Change month"
+          nextMonthLabel="Next month"
+          previousMonthLabel="Previous month"
+          selectionConfiguration={this.state.selectionConfiguration}
+        />
+      </div>
+      <BpkButton onClick={() => alert('It works!')}>Continue</BpkButton>
+    </main>
+    </div>
+    </div>
+    )
+    }
